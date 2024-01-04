6f845400d9bb12af81e983f7fed3c45b716d0ea4
ozvshhh <dlgkdms4660@gmail.com>
2024-01-05 01:59:56 +0900

    home 완성

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import '../styles/home.css';
+import Header from '../components/header'
 
 
 export function Home() {
@@ -40,19 +41,21 @@ export function Home() {
 ])
   return (
     <div className='home'>
-      <div className='title'></div>
+      <Header title={"한잔해"}></Header>
       <div className='search'>
-        <input className='input'></input>
-        <div className='line'></div>
-        <div className='serch_icon'></div>
+        <input className='input' placeholder='원하시는 술을 검색해보세요.'></input>
+        <div className='search_icon'><img src='img/Vector43.png'/></div>
+        <div className='search_icon'><img src='img/Search.png'/></div>
       </div>
       <div className='slider'></div>
-      <div className='contents'>
-        {data.map((key,index)=>{
-          return(
-            <Contents data={data[index]}></Contents>
-          )
-        })}
+      <div className='contents_container'>
+        <div className='contents'>
+          {data.map((key,index)=>{
+            return(
+              <Contents data={data[index]}></Contents>
+            )
+          })}
+        </div>
       </div>
       <div className='nav'></div>
     </div>
@@ -68,10 +71,12 @@ function Contents({data}) {
   return(
     <div className='comContents'>
       <img className='img' src={data.img}/>
-      <div className='name'>{data.name}</div>
-      <div className='percent'>{data.percent}</div>
-      <div className='price_volume'>{data.price}/{data.volume}</div>
-      <div className='tag'>{data.tag}</div>
+      <div className='data_container'>
+        <div className='name'>{data.name}</div>
+        <div className='percent'>{data.percent}</div>
+        <div className='price_volume'>{data.price}/{data.volume}</div>
+        <div className='tag'>{data.tag}</div>
+      </div>
     </div>
   )
 }
