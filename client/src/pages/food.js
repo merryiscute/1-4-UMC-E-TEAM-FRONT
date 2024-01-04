ed67042aba675457e76bac78676bfcdccf243809
wjdalswl <109158284+wjdalswl@users.noreply.github.com>
2024-01-05 07:26:32 +0900

    food, drink 페이지 오류 수정

--- client/src/pages/food.js
+++ client/src/pages/food.js
@@ -43,7 +43,7 @@ export default function Food() {
         <div className="contents">
           {data.map((key, index) => {
             return (
-              <Link to="/write_detail">
+              <Link to="/Food_detail" state={data[index].liquorFood_Id}>
                 <Contents data={data[index]}></Contents>
               </Link>
             );
